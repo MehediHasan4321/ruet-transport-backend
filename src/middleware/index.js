150f114247d946423c8ccb3e608d19cef25b191b
MehediHasan4321 <126mehedi@gmail.como>
2024-06-29 16:55:36 +0600

    added swagger file and setup it

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,16 @@
 const morgan = require('morgan')
 const express = require('express')
 const cors = require('cors')
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocs = YAML.load('./swagger.yaml')
+
+
+
 
 const applyMiddleware = (app)=>{
     app.use([morgan("dev"), cors(), express.json()]);
+    app.use('/docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs))
 }
 
 module.exports=applyMiddleware
